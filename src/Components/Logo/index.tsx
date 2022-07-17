66a7ad3dfde3bd518c59b5795e7c92109f4820eb
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-17 22:07:23 +0100

    Homepage setup

---
+++ src/Components/Logo/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='custom_logo text-3xl'>
+        <span className="font-bold">Logo</span>
+        <span className="block font-[300] text-xl -translate-y-2">here</span>
+    </div>
+  )
+}
+
+export default Logo
