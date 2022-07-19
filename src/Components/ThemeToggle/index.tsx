a1d6ad6482d51dd042165dcf546ca96eb9c6d763
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-19 04:55:11 +0100

    toggle between light mode and dark mode

--- src/Components/ThemeToggle/index.tsx
+++ src/Components/ThemeToggle/index.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import { FaMoon } from 'react-icons/fa'
+import React, { useContext, useState } from 'react'
+import { FaMoon as DarkIcon, FaSun as LightIcon } from 'react-icons/fa'
 
 const ThemeToggle = () => {
+    const [state, setState] = useState(true)
+    /* 
+    true <=> light
+    false <=> dark
+    */
+    const themeToggleHandler = ()=>{
+        setState(!state);
+        ( document.querySelector('html') as HTMLElement).dataset.mode=(state? 'light': 'dark')
+    }
+   
   return (
-    <button>
-        <div className="check">
-            <FaMoon />
+        <button onClick={themeToggleHandler} className='w-[50px] relative h-[30px] rounded-full bg-gray-400 dark:bg-gray-300 transition-colors'>
+        <div className={`transition-colors check w-[25px] h-[25px] text-xs flex justify-center items-center transition-transform rounded-full bg-black text-white dark:bg-white dark:text-black ${state ? 'translate-x-full': ' translate-x-0'}`}>
+            {state ? <DarkIcon />: <LightIcon />}
         </div>
     </button>
   )
--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -3,14 +3,16 @@ import {motion} from 'framer-motion';
 import Logo from '../Components/Logo';
 import NavLinks from '../Components/NavLinks';
 import Button from '../Components/Button';
+import ThemeToggle from '../Components/ThemeToggle';
 
 const Header = () => {
   return (
-    <motion.header className='flex p-6 absolute w-full justify-around items-center'>
+    <motion.header className='flex z-10 p-6 absolute w-full justify-around items-center'>
         <Logo />
        <div className='flex items-center gap-1'>
        <NavLinks />
         <Button className='rounded-full mx-2 p-3'>Try it</Button>
+       <ThemeToggle />
        </div>
     </motion.header>
   )
