74b23460563b3eb42c577cfca8279d13db3498b4
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-19 02:50:43 +0100

    Framer motion setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { Fragment } from "react";
-import { Route, Routes } from "react-router-dom";
+import {AnimatePresence} from 'framer-motion'
+import { Route, Routes, useLocation } from "react-router-dom";
 
 import _404 from "./Pages/_404";
 import Home from "./Pages/Home";
 
 const App = () => {
+  const location= useLocation()
   return (
-    <Fragment>
-      <Routes>
+    <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
 
         <Route path="*" element={<_404 />} />
       </Routes>
-    </Fragment>
+    </AnimatePresence>
   );
 };
 
