b42ec6a593310dd26dc450eb4dfcde3c146b67e1
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-19 04:15:42 +0100

    created nav links and theme toggle button components

---
+++ src/Components/NavLinks/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavLinks = () => {
+  return (
+    <div>NavLinks</div>
+  )
+}
+
+export default NavLinks
---
+++ src/Components/ThemeToggle/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FaMoon } from 'react-icons/fa'
+
+const ThemeToggle = () => {
+  return (
+    <button>
+        <div className="check">
+            <FaMoon />
+        </div>
+    </button>
+  )
+}
+
+export default ThemeToggle
