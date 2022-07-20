dd9a3f60acb66222dba21a4a5da80dc9e34bb6d0
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-20 18:22:51 +0100

    Tweets data

---
+++ src/Services/twitter.ts
@@ -0,0 +1,17 @@
+export default [
+    {
+        username:'xing_xo',
+        displayName:'Akinkumi Io',
+        comment:'Cheers to the HTML Developer!'
+    },
+    {
+        username:'CSS Wizard',
+        displayName:'originaltimi',
+        comment:'100!!'
+    },
+    {
+        username:'always Apear',
+        displayName:'Always Appear',
+        comment:'Amazing Tool bruvvv'
+    },
+]
