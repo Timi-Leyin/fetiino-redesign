c5918241c67fa6b29fa5d35d9a5830f869de862d
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-23 00:30:37 +0100

    removed key prop

--- src/Components/Feature/index.tsx
+++ src/Components/Feature/index.tsx
@@ -4,12 +4,11 @@ interface FeatureProps{
     no:string | number,
     title:string,
     text:string,
-    key?:string,
 }
 
-const Feature = ({no, title,text, key}:FeatureProps) => {
+const Feature = ({no, title,text}:FeatureProps) => {
   return (
-    <motion.div key={key} className="feature py-4">
+    <motion.div key={no} className="feature py-4">
         <motion.span initial={{opacity:0,x:-50}} transition={{delay:.6}} whileInView={{opacity:1, x:0}} className="block font-[600] bg-clip-text text-transparent gradient">{no}</motion.span>
         <motion.h3 initial={{opacity:0,y:-100}} transition={{delay:.3}} whileInView={{opacity:1, y:0}} className="text-2xl font-[400]">{title}</motion.h3>
         <motion.p initial={{opacity:0,y:100}} transition={{delay:.3}} whileInView={{opacity:1, y:0}} className="text-xs w-[300px]">{text}</motion.p>
--- src/Layout/Features.tsx
+++ src/Layout/Features.tsx
@@ -6,9 +6,9 @@ const Features = () => {
    <motion.section key='features' className='flex md:justify-around justify-center items-center py-8 flex-col md:flex-row'>
        <motion.div initial={{y:50,opacity:0}} key="feature-box" transition={{delay:.4}} whileInView={{opacity:1,y:0}} className="w-[400px] h-[400px] gradient rounded-2xl"></motion.div>
        <div className="">
-          <Feature no='01' key="01" title='Brain Storming' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quo.' />
-          <Feature no='02' key="02" title='Understandable' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quo.' />
-          <Feature no='03' key="03" title='Exportable' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quo.' />
+          <Feature no='01' title='Brain Storming' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quo.' />
+          <Feature no='02' title='Understandable' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quo.' />
+          <Feature no='03' title='Exportable' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quo.' />
        </div>
    </motion.section>
   )
