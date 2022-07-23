ed77b0468c26cf88351067094c964ed761c1afa5
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-23 19:37:34 +0100

    add more tweets

--- src/Services/twitter.ts
+++ src/Services/twitter.ts
@@ -2,16 +2,36 @@ export default [
     {
         username:'originaltimi',
         displayName:'CSS Wizard',
-        comment:'Another day to become word class'
+        comment:"I'm bad at color until i discovered @fetiino"
     },
      {
         username:'xing0x',
         displayName:'Akinkumi Io',
-        comment:'hehe, Proud of Myself...'
+        comment:'I feel like an idiot for not creating @fetiino earlier...'
     },
     {
         username:'alwaysappear',
         displayName:'Always Appear',
-        comment:'Amazing Tool bruvvv'
+        comment:'Skip to the end. use Fetiino'
+    },
+    {
+        username:'Nzaki ',
+        displayName:'NzakiCodes',
+        comment:'Doing more with colors'
+    },
+    {
+        username:'1Madvirus',
+        displayName:'Edwin Ayabie',
+        comment:'Doing more with colors'
+    },
+    {
+        username:'PraiseCodes',
+        displayName:'Praise Codes',
+        comment:'Amazing web App'
+    },
+    {
+        username:'obiabo_immanuel',
+        displayName:'Sudo Whoami',
+        comment:'Nice one @xing0x'
     },
 ]
