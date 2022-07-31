d68190ebc1d6574d0a3fb7bd6134dd4703e3a9fc
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-31 14:07:57 +0100

    Created Color Generator route

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,48 @@
 import { Fragment, lazy, Suspense } from "react";
-import {AnimatePresence} from 'framer-motion'
+import { AnimatePresence } from "framer-motion";
 import { Route, Routes, useLocation } from "react-router-dom";
-import 'locomotive-scroll/dist/locomotive-scroll.min.css'
+import "locomotive-scroll/dist/locomotive-scroll.min.css";
 import Preloader from "./Components/Preloader";
 
-const Home = lazy(()=> import('./Pages/Home'))
-const _404 = lazy(()=> import("./Pages/_404"));
+const Home = lazy(() => import("./Pages/Home"));
+const _404 = lazy(() => import("./Pages/_404"));
+const ColorGenerator = lazy(() => import("./Pages/ColorGenerator"));
 
 const App = () => {
-  const storage = localStorage.getItem('theme')
-    if(!storage) localStorage.setItem('theme','light');
-    (document.querySelector('html') as HTMLElement).dataset.mode=(storage || 'light');
-  const location= useLocation()
+  const storage = localStorage.getItem("theme");
+  if (!storage) localStorage.setItem("theme", "light");
+  (document.querySelector("html") as HTMLElement).dataset.mode =
+    storage || "light";
+  const location = useLocation();
   return (
     <AnimatePresence exitBeforeEnter>
       <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Suspense fallback={<Preloader />}>
-          <Home />
-        </Suspense>
-          } />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<Preloader />}>
+              <Home />
+            </Suspense>
+          }
+        />
 
-        <Route path="*" element={<Suspense fallback={<Preloader />}>
-          <_404 />
-        </Suspense>} />
+        <Route
+          path="/colorGenerator"
+          element={
+            <Suspense fallback={<Preloader />}>
+              <ColorGenerator />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<Preloader />}>
+              <_404 />
+            </Suspense>
+          }
+        />
       </Routes>
     </AnimatePresence>
   );
---
+++ src/Pages/ColorGenerator.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import { Helmet } from 'react-helmet'
+
+const ColorGenerator = () => {
+  return (
+    <Fragment>
+    <Helmet>
+      <title>Fetiino - Color Generator</title>
+    </Helmet>
+    <h1>Color Generator</h1>
+    </Fragment>
+  )
+}
+
+export default ColorGenerator
