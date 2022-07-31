6915f83dc10ae96ab42fec11752d24c7edebd210
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-31 13:52:49 +0100

    Fixed Preloader

--- src/Components/Preloader/index.tsx
+++ src/Components/Preloader/index.tsx
@@ -11,9 +11,9 @@ const Preloader = ({onComplete}:PreloaderProps) => {
       key="preloader"
       exit={{ opacity: 0 }}
       id="preloader"
-      className="h-screen select-none pointer-events-none w-full flex-center"
+      className="h-screen select-none pointer-events-none w-full flex"
     >
-      <div className="self-end text-2xl justify-self-left p-12">
+      <div className="text-2xl justify-self-left m-12">
         <motion.div className="" initial={{rotate:0}} animate={{rotate:'360deg'}} transition={{repeat:Infinity, duration:0.9}}>
           <FaSpinner className='spinning' />
         </motion.div>
