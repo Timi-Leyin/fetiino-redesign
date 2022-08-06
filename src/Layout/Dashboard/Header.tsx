46bf616615b0c3eb9489329a5fa0dcb77cc7f85c
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-06 16:48:13 +0100

    Added Theme Toggle

--- src/Layout/Dashboard/Header.tsx
+++ src/Layout/Dashboard/Header.tsx
@@ -1,19 +1,22 @@
-import { FaHeart } from "react-icons/fa"
-import Search from "./Search"
+import { FaHeart } from "react-icons/fa";
+import ThemeToggle from "../../Components/ThemeToggle";
+import Search from "./Search";
 
 const Header = () => {
   return (
-    <div className='flex justify-between items-center'>
-        <Search />
+    <div className="flex justify-between items-center">
+      <Search />
 
+      <div className="flex gap-x-3 items-center">
+        <ThemeToggle />
 
-
-        <div className='bg-gray-200 py-2 p-4 flex items-center gap-3 cursor-pointer rounded-full'>
-            <FaHeart className="text-red-500 " />
-            <span>Favourites</span>
+        <div className="bg-gray-200 py-2 p-4 flex items-center gap-3 cursor-pointer rounded-full dark:bg-gray-800">
+          <FaHeart className="text-red-500 " />
+          <span>Favourites</span>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
