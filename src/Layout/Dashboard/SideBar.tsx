ec834c08568e2189842504c96b733b19f733f4eb
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-06 16:55:29 +0100

    Fix: Added Sidebar item

--- src/Layout/Dashboard/SideBar.tsx
+++ src/Layout/Dashboard/SideBar.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
+import { FaPalette } from 'react-icons/fa'
 import Logo from '../../Components/Logo'
+
+
+interface SideBarItemProps{
+  title: string,
+  icon:ReactNode
+}
+
+const SideBarItem =({title,icon}:SideBarItemProps)=>{
+  return(
+    <li className="my-5" title={title}>
+    <span>{icon}</span>
+  </li>
+  )
+}
+
 
 const SideBar = () => {
   return (
-   <aside className='w-[250px] py-1'>
+   <aside className='w-[auto] py-1'>
        <Logo />
+
+       <ul className='flex flex-col justify-centerr items-center'>
+        <SideBarItem title='Color Generator' icon={<FaPalette />} />
+        <SideBarItem title='Color Generator' icon={<FaPalette />} />
+        <SideBarItem title='Color Generator' icon={<FaPalette />} />
+        <SideBarItem title='Color Generator' icon={<FaPalette />} />
+       </ul>
    </aside>
   )
 }
