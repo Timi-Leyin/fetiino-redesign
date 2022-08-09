21b9d63f91d3cc3a185c04567c62247c886e0734
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-09 20:17:17 +0100

    Fix: text

--- src/Pages/Extract.tsx
+++ src/Pages/Extract.tsx
@@ -8,21 +8,21 @@ const Extract = () => {
        <div className="flex gap-3">
 
     <div className="">
-        <h2 className='text-3xl'>Upload the Image</h2>
-        <h2 className='text-3xl'>You Want to Extract Color From</h2>
+        <h2 className='text-3xl'>Upload Image</h2>
+        {/* <h2 className='text-3xl'>You Want to Extract Color From</h2> */}
         <p className='text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, placeat?</p>
 
 
         <div className="p-12 ring-1 ring-gray-200 m-4 min-h-[300px] w-full flex items-center flex-col text-center">
 
-    <span className='text-4xl'><FaFileImage /></span>
+    <span className='text-4xl opacity-50 block py-3'><FaFileImage /></span>
    <div className="text-xs py-1">
    <p>Drag & Drop Image</p>
     <p> or </p>
    </div>
     <button className='bg-primary px-4 text-white py-2 my-2 text-xs'>Browse</button>
 
-    <p className='text-xs py-2'>
+    <p className='text-xs py-2 w-[200px]'>
         Upload images only in (*.png .jpg .jpeg .gif) format.
     </p>
         </div>
