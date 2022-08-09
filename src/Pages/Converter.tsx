cca6be4d7f51b11be4ac6a5810706680b7e1710b
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-09 19:39:17 +0100

    Feat: add input to color converter page

--- src/Pages/Converter.tsx
+++ src/Pages/Converter.tsx
@@ -4,7 +4,13 @@ const Converter = () => {
   return (
      <DashboardLayout title='Color Format Converter' paragraph='Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere iusto.
     '> 
-  
+        <div className="flex items-center justify-center gap-2">
+            <div className="h-10 w-10 rounded-lg bg-yellow-400"></div>
+            <div>
+                <label className='block absolute text-xs translate-x-2 -translate-y-2  px-[6px] rounded-full bg-gray-800 text-white' htmlFor="color">Color</label>
+                <input type="text" className='p-4 bg-gray-800 text-xs w-[500px] text-white rounded-full' placeholder="#FFF001" />
+            </div>
+        </div>
     </DashboardLayout>
   )
 }
