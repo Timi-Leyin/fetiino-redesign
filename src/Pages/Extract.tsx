edd19bc7f2fabf203dc88fdd93d2658500356f19
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-09 20:48:41 +0100

    Feat: Added color card for color palette

--- src/Pages/Extract.tsx
+++ src/Pages/Extract.tsx
@@ -1,11 +1,11 @@
-import { FaFileImage, FaFileImport } from 'react-icons/fa'
+import { FaCopy, FaFileExport, FaFileImage, FaFileImport, FaHeart } from 'react-icons/fa'
 import DashboardLayout from '../Layout/Dashboard/DashboardLayout'
 
 const Extract = () => {
   return (
     <DashboardLayout title='Color Extract' paragraph='Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere iusto.
     '> 
-       <div className="flex gap-3">
+       <div className="flex justify-between px-6 gap-10">
 
     <div className="">
         <h2 className='text-3xl'>Upload Image</h2>
@@ -34,7 +34,36 @@ const Extract = () => {
 
 
     <div className="">
-        d
+        <div className="pb-3">
+            <h3 className='text-3xl'>Dominant Color</h3>
+        </div>
+
+    {/* color card */}
+
+    <div className='p-2 flex justify-between my-2 items-center bg-yellow-400 font-bold text-xs'>
+        <span className='text-yellow-900'>#FFE001</span>
+        <span className='cursor-pointer text-gray-700  inline-block shadow-sm'>
+            <FaCopy />
+        </span>
+    </div>
+
+{/* ** */}
+    {/* color card */}
+
+    <div className='p-2 flex justify-between my-2 items-center bg-green-400 font-bold text-xs'>
+        <span className='text-green-900'>#EED001</span>
+        <span className='cursor-pointer text-gray-700  inline-block shadow-sm'>
+            <FaCopy />
+        </span>
+    </div>
+
+{/* ** */}
+
+<div className="mt-12 flex flex-col gap-2">
+    <button className='p-3 bg-primary text-white text-xs flex gap-x-2 items-center'><FaFileExport /> {' '} <span>Export Palette</span></button>
+    <button className='p-3 bg-gray-900 text-white text-xs flex gap-x-2 items-center'><FaHeart /> {' '} <span>Save Palette</span></button>
+</div>
+
     </div>
 
 
