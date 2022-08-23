eed1dd1502afe31b35be122be9e4bdb6f4ddc00d
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-23 13:26:05 +0100

    fix: naviagtion

--- src/Layout/Dashboard/Navigation.tsx
+++ src/Layout/Dashboard/Navigation.tsx
@@ -1,41 +1,55 @@
-import React, { ReactNode } from 'react'
-import { FaAd, FaAirbnb, FaCampground, FaHome, FaTachometerAlt, FaUber, FaUbuntu } from 'react-icons/fa'
+import React, { ReactNode, useState } from 'react'
+import { FaAd, FaAirbnb, FaBars, FaCampground, FaHome, FaTachometerAlt, FaUber, FaUbuntu } from 'react-icons/fa'
 import { MdHdrAutoSelect } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 import Logo from '../../Components/Logo'
-
+import {motion} from 'framer-motion'
 
 interface NavItemProps{
   title: string,
   active?:boolean,
   to:string,
-  icon:ReactNode
+  icon:ReactNode,
+  delay:number,
+  toggle:boolean
 }
 
-const NavItem =({title,icon,to,active=false}:NavItemProps)=>{
+const NavItem =({title,icon,to,toggle,delay, active=false}:NavItemProps)=>{
   return(
-  <Link to={to}>
-      <li className={`${active && 'bg-gray-300 dark:bg-gray-800 opacity-100 text-primary' }  flex-shrink-0  flex items-center justify-center transition-all hover:opacity-100 opacity-70  gap-2 my-1 text-lg cursor-pointer p-3  rounded-full dark `} title={title}>
+  <Link to={to} className={`${!toggle && 'pointer-events-none'}`}>
+      <motion.li animate={{opacity:toggle?1:0}} transition={{delay:delay}} className={`cursor-pointer select-none bg-gray-300/60 filter-bg-blur dark:bg-gray-800/60  flex-shrink-0  flex items-center justify-around transition-all hover:opacity-100  gap-2 my-1 text-lg p-3  rounded-full dark `} title={title}>
     <span className=''>{icon}</span>
-    <span className="text-xs">{title}</span>
-  </li>
+    {/* <span className="text-xs">{title}</span> */}
+  </motion.li>
   </Link>
   )
 }
 
 
 const Navigation = () => {
+  const [menu, setMenu] = useState(false)
   return (
-   <aside className=' relative translate-y-[6.8rem] mx-[5%] '>
-       <ul className='flex  justify-around w-full mx-auto gap-4  overflow-x-auto  items-center'>
-        <NavItem to='/explore' title='Explore Palette' active={true} icon={<FaTachometerAlt />} />
-        <NavItem to='/explore' title='Color Picker' icon={<FaAirbnb />} />
-        <NavItem to='/explore' title='Gradient Maker' icon={<FaCampground />} />
-        <NavItem to='/explore' title='Explore Colors' icon={<FaUber />} />
-        <NavItem to='/explore' title='Extract Colors' icon={<FaAd />} />
-        <NavItem to='/explore' title='Saved Palette' icon={<FaUbuntu />} />
+  <section className=''>
+
+    <div onClick={()=> setMenu(!menu)} className='h-[70px] w-[70px] flex items-center justify-center hover:text-primary rounded-full bg-primary fixed bottom-0 right-0 m-7 z-10 shadow-lg cursor-pointer hover:bg-white transition-colors'>
+    <FaBars />
+      </div>
+
+
+
+<motion.aside className=' fixed z-10 bottom-[10%] right-0 m-5  mx-[5%] '>
+       <ul className='flex flex-col  relative mx-auto gap-4  overflow-x-auto  items-center'>
+         <NavItem to='/generate' toggle={menu} delay={1} title='Explore Palette' active={true} icon={<FaTachometerAlt />} />
+
+        <NavItem to='/colorPicker'  toggle={menu} delay={0.8} title='Color Picker' icon={<FaAirbnb />} />
+        <NavItem to='/gradient-maker' toggle={menu} delay={0.6} title='Gradient Maker' icon={<FaCampground />} />
+        <NavItem to='/converter' toggle={menu} delay={0.4} title='Explore Colors' icon={<FaUber />} />
+        <NavItem to='/extract' toggle={menu} delay={0.2} title='Extract Colors' icon={<FaAd />} />
+        <NavItem to='/favourites' toggle={menu} delay={0.1} title='Saved Palette' icon={<FaUbuntu />} />
        </ul>
-   </aside>
+   </motion.aside>
+
+  </section>
   )
 }
 
